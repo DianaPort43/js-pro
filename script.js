d97d2db2a24cfa139e5450a6175f81029b100a78
DianaPort43 <129172905+DianaPort43@users.noreply.github.com>
2023-06-08 22:26:47 +0600

    first code JS

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+// PLAYERCHOICES
+var choices =["✊", "✋", "✌"];
+
+
+
+function selectChoice(index){
+    var playerSign = document.querySelector('#playerSign')
+    playerSign.innerHTML=choices[index];
+
+    var choiceElements = document.querySelector('.btn-play');
+    for (var i = 0; i < choiceElements.length; i++) {
+      if (i !== index) {
+        choiceElements[i].classList.add('dimmed');
+      }
+    }
+    setTimeout(function() {
+        for (var i = 0; i < choiceElements.length; i++) {
+            choiceElements[i].classList.remove('dimmed');
+        }
+        playerSign.innerHTML = '❔';
+      }, 2000);
+}
+
+// COMPUTER CHOICES
+
+function getComputerChoice(randomIndex) {
+    var randomIndex = Math.floor(Math.random() * choices.length);
+    computerSign.innerHTML = choices[randomIndex];
+
+    var compChoiceElements = document.querySelector('.btn');
+    for (var i = 0; i < compChoiceElements.length; i++) {
+      if (i !== index) {
+        compChoiceElements[i].classList.add('dimmed');
+      }
+    }
+    setTimeout(function() {
+        for (var i = 0; i < compChoiceElements.length; i++) {
+            compChoiceElements[i].classList.remove('dimmed');
+        }
+        computerSign.innerHTML = '❔';
+      }, 2000);
+}
 
